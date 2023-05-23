ff09d7565c8c95bbad8e459ca58dda223d952d54
Sarmistha Pal <sarmistha15-9306@diu.edu.bd>
2023-05-23 09:13:13 +0600

    Add files via upload

---
+++ Q&A.py
@@ -0,0 +1,28 @@
+import os
+import openai
+import pyttsx3
+
+
+
+openai.api_key = "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"
+model_engine = "text-davinci-003"
+p = input("Question:")
+
+response = openai.Completion.create(
+  model=model_engine,
+  prompt=p,
+  max_tokens=1000,
+  top_p=1,
+  stop=None,
+  temperature=0.5,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
+
+r = response.choices[0].text
+print("Answer:"+r)
+
+test_to_speech = pyttsx3.init()
+speech = r
+test_to_speech.say(speech)
+test_to_speech.runAndWait()
